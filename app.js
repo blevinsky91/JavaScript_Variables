1a3b1e55883beb84c957de62306d8548d1e3f50e
Brandon Levinsky <blevinsky91@gmail.com>
2023-07-26 15:30:53 -0400

    ex completed

--- app.js
+++ app.js
@@ -5,6 +5,51 @@ console.log(
 
 // Exercise 1
 
+//const firstName; //needs to be assigned
+
+let lastName;
+
+var age;
+
 // Exercise 2
 
+const firstName = "Brandon"; //assigned (from ex1)
+
+lastName = "Levinsky";
+
+age = 31;
+
 // Exercise 3
+
+let language = "JavaScript";
+
+let createdYear = 1995;
+
+let isCaseSensitive = true;
+
+// Exercise 4
+
+let price = 19.99;
+
+let isOnSale = false;
+
+let salePercentage = 15;
+
+let stock = 0;
+
+let inStock = false;
+
+let selectedSize = "M";
+
+// Exercise 5
+
+let title = "Name of the Wind";
+
+let author = "Patrick Rothfuss";
+
+let pageCount = 722;
+
+let bookmark = 456;
+
+let hasRead = true;
+
